cd37f1db12f73c7292f50201a94e904b27b969ce
snowaver <1123009419@qq.com>
2019-03-12 15:34:57 +0800

    1/resolve custom sneaker view displaying error.

--- widget/src/main/java/cc/mashroom/hedgehog/parent/AbstractActivity.java
+++ widget/src/main/java/cc/mashroom/hedgehog/parent/AbstractActivity.java
@@ -5,6 +5,7 @@ import  android.content.Intent;
 import  android.os.Bundle;
 import  android.view.LayoutInflater;
 import  android.view.View;
+import  android.view.ViewGroup;
 import  android.widget.ImageView;
 import  android.widget.TextView;
 
@@ -15,6 +16,7 @@ import  androidx.appcompat.app.AppCompatActivity;
 import  java.util.LinkedList;
 
 import  cc.mashroom.hedgehog.R;
+import  cc.mashroom.hedgehog.util.ContextUtils;
 import  cc.mashroom.util.ObjectUtils;
 
 public  abstract  class  AbstractActivity  extends  AppCompatActivity
@@ -52,15 +54,24 @@ public  abstract  class  AbstractActivity  extends  AppCompatActivity
 		e.printStackTrace( );
 	}
 
-
-	public  Sneaker  setSneakerView( Sneaker  sneaker, int  icon, int  title, int  titleColor )
+	public  void  showSneakerWindow( Sneaker  sneaker,int  iconResId,int  titleResId,int  titleColorResId,int  backgroundColorResId )
 	{
-		View  sneakerView = LayoutInflater.from(this).inflate( R.layout.sliding_sneaker,sneaker.getView(),false );
+		View  sneakerView  = LayoutInflater.from(this).inflate( R.layout.sliding_sneaker,sneaker.getView(),false );
 
-		ObjectUtils.cast(sneakerView.findViewById(R.id.icon),ImageView.class).setImageResource(   icon );
+		ObjectUtils.cast(sneakerView.findViewById(cc.mashroom.hedgehog.R.id.icon),ImageView.class).setImageResource( iconResId );
 
-		ObjectUtils.cast(sneakerView.findViewById(R.id.title),TextView.class).setText( title );
+		ObjectUtils.cast(sneakerView.findViewById(cc.mashroom.hedgehog.R.id.title),TextView.class).setText( titleResId );
 
-		ObjectUtils.cast(sneakerView.findViewById(R.id.title),TextView.class).setTextColor( titleColor );  sneaker.sneakCustom( sneakerView );  return  sneaker;
+		ObjectUtils.cast(sneakerView.findViewById(cc.mashroom.hedgehog.R.id.title),TextView.class).setTextColor( super.getResources().getColor(titleColorResId) );
+
+		sneaker.autoHide(true).setDuration( 3000 );
+
+		ViewGroup.LayoutParams  layoutParams = sneakerView.findViewById(R.id.head_content).getLayoutParams();
+
+		layoutParams.height = ContextUtils.getStatusBarHeight(this );
+
+		sneakerView.setLayoutParams(layoutParams );
+
+		sneakerView.setBackgroundColor( super.getResources().getColor( backgroundColorResId ) );  sneaker.sneakCustom( sneakerView );
 	}
 }
