8ce1aabe9da6a42ab10e1d2775eb3637ac4d51fe
snowaver <1123009419@qq.com>
2019-08-20 12:47:32 +0800

    1/format codes.

--- widget/src/main/java/cc/mashroom/hedgehog/widget/StyleableEditView.java
+++ widget/src/main/java/cc/mashroom/hedgehog/widget/StyleableEditView.java
@@ -8,7 +8,7 @@ import  android.text.InputType;
 import  android.util.AttributeSet;
 import  android.view.LayoutInflater;
 import  android.view.View;
-import android.widget.ImageView;
+import  android.widget.ImageView;
 import  android.widget.RelativeLayout;
 import  android.widget.TextView;
 
