dbb49961a76eff649a7c5799c081dc0d2ff8e00e
snowaver <1123009419@qq.com>
2019-08-22 21:17:07 +0800

    1/use icons instead of text in header bar.

--- widget/src/main/java/cc/mashroom/hedgehog/module/common/activity/ImageCropingActivity.java
+++ widget/src/main/java/cc/mashroom/hedgehog/module/common/activity/ImageCropingActivity.java
@@ -4,7 +4,6 @@ import  android.content.Intent;
 import  android.graphics.Bitmap;
 import  android.net.Uri;
 import  android.os.Bundle;
-import  android.widget.TextView;
 
 import  com.irozon.sneaker.Sneaker;
 import  com.steelkiwi.cropiwa.CropIwaView;
@@ -16,6 +15,7 @@ import  java.net.URI;
 import  cc.mashroom.hedgehog.R;
 import  cc.mashroom.hedgehog.parent.AbstractActivity;
 import  cc.mashroom.hedgehog.util.DensityUtils;
+import  cc.mashroom.hedgehog.widget.ViewSwitcher;
 import  cc.mashroom.util.FileUtils;
 import  cc.mashroom.util.ObjectUtils;
 
@@ -27,7 +27,7 @@ public  class  ImageCropingActivity    extends  AbstractActivity  implements  Cr
 
 		setContentView( R.layout.activity_image_croping  );
 
-		ObjectUtils.cast(super.findViewById(R.id.additional_text),TextView.class).setOnClickListener( (view) -> {try{ ObjectUtils.cast(super.findViewById(R.id.crop_view),CropIwaView.class).crop(new  CropIwaSaveConfig.Builder(Uri.fromFile(FileUtils.createFileIfAbsent(new  File(application().getCacheDir(),"image.png.tmp"),null))).setCompressFormat(Bitmap.CompressFormat.PNG).setQuality(100).setSize(200,200).build()); }catch(Exception  e){ e.printStackTrace(); }} );
+		ObjectUtils.cast(super.findViewById(R.id.additional_switcher),ViewSwitcher.class).setOnClickListener( (view) -> {try{ ObjectUtils.cast(super.findViewById(R.id.crop_view),CropIwaView.class).crop(new  CropIwaSaveConfig.Builder(Uri.fromFile(FileUtils.createFileIfAbsent(new  File(application().getCacheDir(),"image.png.tmp"),null))).setCompressFormat(Bitmap.CompressFormat.PNG).setQuality(100).setSize(200,200).build()); }catch(Exception  e){ e.printStackTrace(); }} );
 
 		float  cropScale = ( (float)  super.getResources().getDisplayMetrics().widthPixels / 2-DensityUtils.px(this,11) )   / ( (float)  super.getResources().getDisplayMetrics().widthPixels / 2 );
 
