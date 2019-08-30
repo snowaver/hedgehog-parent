6ab740d58eaa220df6d37de95c5c7c086e46ae33
snowaver <1123009419@qq.com>
2019-08-31 00:39:49 +0800

    1/correct media type default value to 3 in album media multichoice activity.

--- widget/src/main/java/cc/mashroom/hedgehog/module/common/activity/AlbumMediaMultichoiceActivity.java
+++ widget/src/main/java/cc/mashroom/hedgehog/module/common/activity/AlbumMediaMultichoiceActivity.java
@@ -29,6 +29,7 @@ import  cc.mashroom.util.collection.map.HashMap;
 import  cc.mashroom.util.collection.map.Map;
 import  cn.refactor.library.SmoothCheckBox;
 import  lombok.Setter;
+import  lombok.experimental.Accessors;
 import  permissions.dispatcher.NeedsPermission;
 import  permissions.dispatcher.OnShowRationale;
 import  permissions.dispatcher.PermissionRequest;
@@ -39,7 +40,7 @@ import  java.io.Serializable;
 import  java.util.List;
 
 /**
- *  choose  album  medias.  intent  parameters:  MEDIA_TYPE  (1.photo,  2.video,  3.photo  and  video.  NOTE:  any  one  is  optional  if  absent) ,  MAX_COUNT  (max  count  limitation  of  optional  medias,  no  limitation  if  absent  (default:  -1),  equals  or  less  than  -1.  NOTE:  0  is  illegal.)  and  MAX_FILE_SIZE  (max  file  size,  no  limitation  if  absent  (default:  -1),  equals  or  less  than  -1.  NOTE:  0  is  illegal.).  returns  chosen  medias  (intent  parameters:  MEDIAS)  as  result  data.
+ *  choose  album  medias  and  return  chosenes  (intent  parameters:  MEDIAS)  by  result  data.  intent  parameters:  MEDIA_TYPE  (1.photo,  2.video,  3.photo  and  video.  other  values  are  illegal.  default  3),  MAX_COUNT  (max  chosen  medias  count,  zero  is  illegal  and  negative  for  no  limitation.  default:  -1)  and  MAX_FILE_SIZE  (max  file  size,  zero  is  illegal  and  negative  for  no  limitation.  default:  -1).
  */
 @RuntimePermissions
 
@@ -52,14 +53,30 @@ public  class  AlbumMediaMultichoiceActivity    extends  AbstractActivity  imple
 		application().getMainLooperHandler().post( () -> StyleUnifier.unify(new  UIAlertDialog.DividerIOSBuilder(this).setBackgroundRadius(15).setTitle(R.string.notice).setTitleTextSize(18).setMessage(R.string.album_require_album_permission).setMessageTextSize(18).setCancelable(false).setCanceledOnTouchOutside(false).setNegativeButtonTextSize(18).setNegativeButton(R.string.close,(dialog,which) -> {permissionRequest.cancel();  ContextUtils.finish(this);}).setPositiveButtonTextSize(18).setPositiveButton(R.string.ok,(dialog,which) -> permissionRequest.proceed()).create().setWidth((int)  (super.getResources().getDisplayMetrics().widthPixels*0.9)),Typeface.createFromAsset(super.getAssets(),"font/droid_sans_mono.ttf")).show() );
 	}
 
+	@Accessors( chain = true )
+	@Setter
+	protected  int   mediaType   =  3;
 	@Setter
 	protected  boolean  initialized = false;
+    @Accessors( chain = true )
+    @Setter
+    protected  int   maxCount    = -1;
+    @Accessors( chain = true )
+    @Setter
+    protected  long  maxFileSize = -1;
 
 	protected  void  onCreate(      Bundle  savedInstanceState )
 	{
 		super.onCreate(savedInstanceState );
 
 		super.setContentView(R.layout.activity_album_media_multichoice );
+
+		this.setMediaType(super.getIntent().getIntExtra("MEDIA_TYPE",-1)).setMaxCount(super.getIntent().getIntExtra("MAX_COUNT",-1)).setMaxFileSize( super.getIntent().getLongExtra("MAX_FILE_SIZE", -1) );
+
+		if( mediaType != 1 && mediaType != 2 && mediaType != 3 )
+        {
+            throw  new  IllegalArgumentException( String.format("HEDGEHOG-PARENT:  ** ALBUM  MEDIA  MULTICHOICE  ACTIVITY **  media  type  (%d)  is  illegal.",   mediaType) );
+        }
 
 		super.findViewById(R.id.additional_switcher).setOnClickListener( (view) -> StyleUnifier.unify(new  UIActionSheetDialog.ListIOSBuilder(this).setBackgroundRadius(15).addItem(R.string.photo).addItem(R.string.video).addItem(R.string.album_photo_and_video).setItemsTextSize(18).setCancel(R.string.close).setCancelTextColorResource(R.color.red).setCancelTextSize(18).setItemsMinHeight(DensityUtils.px(this,50)).setPadding(DensityUtils.px(this,10)).setCanceledOnTouchOutside(true).setOnItemClickListener(this).create(),Typeface.createFromAsset(super.getAssets(),"font/droid_sans_mono.ttf")).show() );
 
@@ -87,14 +104,14 @@ public  class  AlbumMediaMultichoiceActivity    extends  AbstractActivity  imple
 	{
 		initialized    = true;
 
-		ObjectUtils.cast(super.findViewById(R.id.header_bar),HeaderBar.class).setTitle( super.getString(titles.get(super.getIntent().getIntExtra("MEDIA_TYPE",3))) );
+		ObjectUtils.cast(super.findViewById(R.id.header_bar),HeaderBar.class).setTitle( super.getString(this.titles.get(this.mediaType)) );
 
-		super.findViewById(R.id.additional_switcher).setVisibility(  super.getIntent().hasExtra("MEDIA_TYPE") ? View.GONE : View.VISIBLE );
+		super.findViewById(R.id.additional_switcher).setVisibility( mediaType == 1 || mediaType == 2 ? View.GONE : View.VISIBLE );
 
-		ObjectUtils.cast(super.findViewById(R.id.album_media_list),ListView.class).setAdapter( new  AlbumMediaMultichoiceListviewAdapter( this, super.getIntent().getIntExtra("MEDIA_TYPE",3) , 3, super.getIntent().getIntExtra("MAX_COUNT",-1) , this ) );
+		ObjectUtils.cast(super.findViewById(R.id.album_media_list),ListView.class).setAdapter( new  AlbumMediaMultichoiceListviewAdapter( this, mediaType,3,maxCount,maxFileSize,this) );
 	}
 
-	private  Map<Integer,Integer>  titles = new  HashMap<Integer,Integer>().addEntry(1,R.string.photo).addEntry(2,R.string.video).addEntry( 3 , R.string.album_photo_and_video );
+	private  Map<Integer,Integer>  titles = new  HashMap<Integer,Integer>().addEntry(1,R.string.photo).addEntry(2,R.string.video).addEntry( 3,R.string.album_photo_and_video );
 
 	@UiThread
 	public  void  onRequestPermissionsResult( int  requestCode, @NonNull  String[]  permissions, @NonNull  int[]  grantedResults )
@@ -111,9 +128,9 @@ public  class  AlbumMediaMultichoiceActivity    extends  AbstractActivity  imple
 
 	public  void  onClick( BasisDialog  dialog,View  view,int  position )
 	{
-		ObjectUtils.cast(super.findViewById(R.id.header_bar),HeaderBar.class).setTitle( super.getString(titles.get(position+1)) );
+		ObjectUtils.cast(super.findViewById(R.id.header_bar),HeaderBar.class).setTitle(     super.getString(this.titles.get(position+1)) );
 
-		ObjectUtils.cast(super.findViewById(R.id.album_media_list),ListView.class).setAdapter( new  AlbumMediaMultichoiceListviewAdapter( this,position+1,3,super.getIntent().getIntExtra("LIMITATION",3),this ) );
+		ObjectUtils.cast(super.findViewById(R.id.album_media_list),ListView.class).setAdapter(          new  AlbumMediaMultichoiceListviewAdapter(this,position+1,3,this.maxCount,this.maxFileSize,this) );
 	}
 
 	public  void  onCheckedChanged( SmoothCheckBox compoundButton,boolean checked )
@@ -122,7 +139,7 @@ public  class  AlbumMediaMultichoiceActivity    extends  AbstractActivity  imple
 
 		List<Media>  choosedMedias  = ObjectUtils.cast(listview.getAdapter(),AlbumMediaMultichoiceListviewAdapter.class).getChosenMedias();
 
-		if( super.getIntent().getIntExtra("MAX_COUNT",-1) == 1&&checked )
+		if( this.maxCount == 1  && checked )
 		{
 			int  firstVisiblePosition=listview.getFirstVisiblePosition();
 
--- widget/src/main/java/cc/mashroom/hedgehog/module/common/activity/EditorActivity.java
+++ widget/src/main/java/cc/mashroom/hedgehog/module/common/activity/EditorActivity.java
@@ -16,6 +16,9 @@ import  cc.mashroom.util.ObjectUtils;
 import  cc.mashroom.util.StringUtils;
 import  cc.mashroom.hedgehog.widget.HeaderBar;
 
+/**
+ *  edit  the  content  and  returns  edit  content  (intent  parameters:  CONTENT)  as  result  data.  intent  parameters:  TITLE  (title  on  headbar),  CONTENT  (content  to  be  edited)  AND  MAX_COUNT  (max  character  count,  zero  is  illegal  and  negative  for  no  limitation).
+ */
 public  class  EditorActivity  extends  AbstractActivity  implements  View.OnClickListener,TextWatcher
 {
 	public  void  beforeTextChanged( CharSequence  text,int  start,int  count,int  after )
@@ -27,7 +30,7 @@ public  class  EditorActivity  extends  AbstractActivity  implements  View.OnCli
 	{
 		if( StringUtils.isNotBlank(ObjectUtils.cast(super.findViewById(R.id.edit),EditText.class).getText().toString().trim()) )
 		{
-			if( this.limitation == -1 || ObjectUtils.cast(super.findViewById(R.id.edit),EditText.class).getText().length() >= limitation+1 )
+			if( this.maxCount == -1 || ObjectUtils.cast(super.findViewById(R.id.edit),EditText.class).getText().length() >= maxCount+1 )
 			{
 				super.showSneakerWindow( Sneaker.with(this),com.irozon.sneaker.R.drawable.ic_error,R.string.content_length_out_of_limitation,R.color.white,R.color.red );
 			}
@@ -42,7 +45,7 @@ public  class  EditorActivity  extends  AbstractActivity  implements  View.OnCli
 		}
 	}
 
-	private  int  limitation = -1;
+	private  int  maxCount = -1;
 
 	protected  void  onCreate( Bundle  savedInstanceState )
 	{
@@ -50,9 +53,9 @@ public  class  EditorActivity  extends  AbstractActivity  implements  View.OnCli
 
 		super.setContentView(   R.layout.activity_editor );
 
-		ObjectUtils.cast(super.findViewById(R.id.header_bar),HeaderBar.class).setTitle( super.getIntent().hasExtra("TITLE") ? super.getIntent().getStringExtra("TITLE") : super.getString(R.string.edit) );
+		ObjectUtils.cast(super.findViewById(R.id.header_bar),HeaderBar.class).setTitle(    super.getIntent().hasExtra("TITLE") ? super.getIntent().getStringExtra("TITLE") : super.getString(R.string.edit) );
 
-		this.limitation = super.getIntent().getIntExtra( "LIMITATION",-1 );
+		this.maxCount = super.getIntent().getIntExtra("MAX_COUNT",-1 );
 
 		String  content = !super.getIntent().hasExtra("CONTENT") ? "" : super.getIntent().getStringExtra( "CONTENT" );
 
@@ -67,15 +70,15 @@ public  class  EditorActivity  extends  AbstractActivity  implements  View.OnCli
 
 	public  void  afterTextChanged(Editable  textEditable )
 	{
-		ObjectUtils.cast(super.findViewById(R.id.notes),TextView.class).setTextColor( super.getResources().getColor(this.limitation == -1 || textEditable.length() <= this.limitation ? R.color.darkgray : R.color.red) );
+		ObjectUtils.cast(super.findViewById(R.id.notes),TextView.class).setTextColor( super.getResources().getColor(this.maxCount == -1 || textEditable.length() <= this.maxCount ? R.color.darkgray : R.color.red) );
 
-		ObjectUtils.cast(super.findViewById(R.id.notes),TextView.class).setText( this.limitation == -1 ? ""+textEditable.length() : StringUtils.rightPad(""+textEditable.length(),Math.max(2,(limitation+"").length())," ")+" /"+StringUtils.leftPad(""+limitation,Math.max(2,(limitation+"").length())," ") );
+		ObjectUtils.cast(super.findViewById(R.id.notes),TextView.class).setText( this.maxCount <= -1 ? ""+textEditable.length() : StringUtils.rightPad(""+textEditable.length(),Math.max(2,(maxCount+"").length())," ")+" /"+StringUtils.leftPad(""+maxCount,Math.max(2,(maxCount+"").length())," ") );
 	}
 
 	public  void  onTextChanged(   CharSequence  text  ,int  start,int before,int  count )
 	{
-		ObjectUtils.cast(super.findViewById(R.id.notes),TextView.class).setTextColor( super.getResources().getColor(this.limitation == -1 || text.length() <= this.limitation ? R.color.darkgray : R.color.red) );
+		ObjectUtils.cast(super.findViewById(R.id.notes),TextView.class).setTextColor( super.getResources().getColor(this.maxCount == -1 || text.length() <= this.maxCount ? R.color.darkgray : R.color.red) );
 
-		ObjectUtils.cast(super.findViewById(R.id.notes),TextView.class).setText( this.limitation == -1 ? ""+text.length() : StringUtils.rightPad(""+text.length(),Math.max(2,(limitation+"").length())," ")+" /"+StringUtils.leftPad(""+limitation,Math.max(2,(limitation+"").length())," ") );
+		ObjectUtils.cast(super.findViewById(R.id.notes),TextView.class).setText( this.maxCount <= -1 ? ""+text.length() : StringUtils.rightPad(""+text.length(),Math.max(2,(maxCount+"").length())," ")+" /"+StringUtils.leftPad(""+maxCount,Math.max(2,(maxCount+"").length())," ") );
 	}
 }
